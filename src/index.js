0892abaa402e8c0a34de2aee45becc257f90ed66
eduarda.melo <eduarda.melo.ext@synchro.com.br>
2024-08-16 15:26:38 -0300

    minhas transformações

--- src/index.js
+++ src/index.js
@@ -30,6 +30,66 @@ config.tagMapping.forEach((mapping) => {
     destinationTags[i].textContent = sourceTags[i].textContent;
   }
 });
+config.tagConcat.forEach((mapping) => {
+  const destinationTags = modelXmlDoc.getElementsByTagName(mapping.destination);
+
+  for (let i = 0; i < destinationTags.length; i++) {
+    let concatenatedValue = "";
+    mapping.source.forEach((sourceTag) => {
+      const sourceElements = inputXmlDoc.getElementsByTagName(sourceTag);
+      if (sourceElements.length > 0) {
+        concatenatedValue += sourceElements[0].textContent + " "; // Adiciona um espaço como separador
+      }
+    });
+    destinationTags[i].textContent = concatenatedValue.trim(); // Remove espaços em branco extras
+  }
+});
+
+config.tagCondicion.forEach((condition) => {
+  const destinationTags = modelXmlDoc.getElementsByTagName(
+    condition.destination
+  );
+
+  for (let i = 0; i < destinationTags.length; i++) {
+    let meetsCondition = false;
+
+    condition.condicionIgualdadeValor.forEach((cond) => {
+      const [tagToSearch, comparisonValue, valueIfTrue] = cond;
+      const elementToSearch = inputXmlDoc.getElementsByTagName(tagToSearch)[0];
+      if (elementToSearch && elementToSearch.textContent === comparisonValue) {
+        destinationTags[i].textContent = valueIfTrue;
+      }
+    });
+  }
+});
+
+config.tagDiferenca.forEach((condition) => {
+  const destinationTags = modelXmlDoc.getElementsByTagName(
+    condition.destination
+  );
+
+  for (let i = 0; i < destinationTags.length; i++) {
+    const [tagToSearch, comparisonValue] = condition.condicionDiferenca;
+    const elementToSearch = inputXmlDoc.getElementsByTagName(tagToSearch)[0];
+    console.log(comparisonValue);
+    console.log(elementToSearch.textContent);
+
+    if (elementToSearch) {
+      if (elementToSearch.textContent !== comparisonValue) {
+        const valueToAssign = inputXmlDoc.getElementsByTagName(
+          condition.valueIfTrue[0]
+        )[0].textContent;
+        console.log(valueToAssign);
+        destinationTags[i].textContent = valueToAssign;
+      } else {
+        const valueToAssign = inputXmlDoc.getElementsByTagName(
+          condition.valueIfFalse[0]
+        )[0].textContent;
+        destinationTags[i].textContent = valueToAssign;
+      }
+    }
+  }
+});
 
 const serializer = new XMLSerializer();
 const outputXmlString = serializer.serializeToString(modelXmlDoc);
