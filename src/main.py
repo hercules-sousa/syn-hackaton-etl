62eb0515a2871bd39b8af2266694078d7530f99c
AndersonNobrega <anderson.nobrega62@gmail.com>
2024-08-18 18:48:47 -0300

    Add changes to main file

--- src/main.py
+++ src/main.py
@@ -1,13 +1,34 @@
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from lxml import etree
 
+
+def get_args():
+    parser = ArgumentParser(allow_abbrev=False, description='',
+                            formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--xml_file', type=str, default=None)
+
+    return vars(parser.parse_args())
+
+
+args = get_args()
+
 parser = etree.XMLParser(recover=True)
-xml_tree = etree.parse('xml/XML_ORACLE_CLOUD.xml', parser)
-
+xml_tree = etree.parse(args['xml_file'], parser)
 xslt_tree = etree.parse('template.xsl')
-
 transform = etree.XSLT(xslt_tree)
-transformed_tree = transform(xml_tree)
 
-output_file = 'transformed.xml'
-with open(output_file, 'wb') as f:
-    f.write(etree.tostring(transformed_tree, pretty_print=True))
+root = xml_tree.getroot()
+
+if root.tag == 'FISCAL_DOC_HEADER':
+    fiscal_docs = [root]
+else:
+    fiscal_docs = root.findall('FISCAL_DOC_HEADER')
+
+for index, fiscal_doc in enumerate(fiscal_docs):
+    new_tree = etree.ElementTree(fiscal_doc)
+    transformed_tree = transform(new_tree)
+
+    output_file = 'out/transformed_{}.xml'.format(index)
+    with open(output_file, 'wb') as f:
+        f.write(etree.tostring(transformed_tree,
+                               pretty_print=True, xml_declaration=True))
