3cbb68398afa454152b2e0f85afc1db74840def6
AndersonNobrega <anderson.nobrega62@gmail.com>
2024-08-19 22:05:26 -0300

    Add support for custom tags

--- src/main.py
+++ src/main.py
@@ -2,11 +2,19 @@ import os
 import requests
 import io
 import re
+import json
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from lxml import etree
 from dotenv import load_dotenv
 
 
-load_dotenv()
+def get_args():
+    parser = ArgumentParser(allow_abbrev=False, description='',
+                            formatter_class=ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--custom_tags', type=str, help='', default=None)
+
+    return vars(parser.parse_args())
 
 
 def extract_encoding(xml_string):
@@ -19,6 +27,11 @@ def extract_encoding(xml_string):
         return 'UTF-8'
 
 
+load_dotenv()
+
+args = get_args()
+with open(args['custom_tags']) as json_file:
+    custom_tags = json.load(json_file)
 reset_res = requests.post('{}/xml-files/reset'.format(os.getenv('BASE_URL')))
 print(reset_res.content)
 
@@ -28,7 +41,6 @@ for _ in range(12):
     body['id'] = response.json()['id']
     xml_files = response.json()['content']
     xml_encoding = extract_encoding(xml_files)
-    parser = etree.XMLParser(recover=True)
     xml_tree = etree.parse(io.BytesIO(xml_files.encode(xml_encoding)))
     xslt_tree = etree.parse('template.xsl')
     transform = etree.XSLT(xslt_tree)
@@ -44,12 +56,20 @@ for _ in range(12):
         new_tree = etree.ElementTree(fiscal_doc)
         transformed_tree = transform(new_tree)
 
+        ns = {'nfe': 'http://www.synchro.com.br/nfe'}
+
+        rps_element = transformed_tree.find('.//nfe:RPS', namespaces=ns)
+
+        for key, value in custom_tags.items():
+            element = etree.Element(key)
+            element.text = str(value)
+            rps_element.append(element)
+
         body['nfs'].append(etree.tostring(
             transformed_tree).decode(xml_encoding))
 
     post_res = requests.post(
         '{}/xml-files'.format(os.getenv('BASE_URL')), json=body)
-    print(post_res.content)
 
 time_response = requests.get('{}/xml-files/time'.format(os.getenv('BASE_URL')))
 print(time_response.content)
--- src/single_file.py
+++ src/single_file.py
@@ -1,21 +1,48 @@
-
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+import json
 from lxml import etree
 
-parser = etree.XMLParser(recover=True)
-xml_tree = etree.parse(open('xml/XML_ORACLE_CLOUD.xml'), parser)
-xslt_tree = etree.parse('template.xsl')
-transform = etree.XSLT(xslt_tree)
 
-root = xml_tree.getroot()
+def get_args():
+    parser = ArgumentParser(allow_abbrev=False, description='',
+                            formatter_class=ArgumentDefaultsHelpFormatter)
 
-if root.tag == 'FISCAL_DOC_HEADER':
-    fiscal_docs = [root]
+    parser.add_argument('--custom_tags', type=str, help='', default=None)
 
-for index, fiscal_doc in enumerate(fiscal_docs):
-    new_tree = etree.ElementTree(fiscal_doc)
-    transformed_tree = transform(new_tree)
+    return vars(parser.parse_args())
 
-    output_file = 'out/transformed_{}.xml'.format(index)
-    with open(output_file, 'wb') as f:
-        f.write(etree.tostring(transformed_tree,
-                pretty_print=True, xml_declaration=True))
+
+if __name__ == "__main__":
+    args = get_args()
+    with open(args['custom_tags']) as json_file:
+        custom_tags = json.load(json_file)
+
+    parser = etree.XMLParser(recover=True)
+    xml_tree = etree.parse(open('xml/XML_Nfe_125176099_2024-08-02_15_08_379.xml'), parser)
+    xslt_tree = etree.parse('template.xsl')
+    transform = etree.XSLT(xslt_tree)
+
+    root = xml_tree.getroot()
+
+    if root.tag == 'FISCAL_DOC_HEADER':
+        fiscal_docs = [root]
+    else:
+        fiscal_docs = root.findall('FISCAL_DOC_HEADER')
+
+    for index, fiscal_doc in enumerate(fiscal_docs):
+        new_tree = etree.ElementTree(fiscal_doc)
+        transformed_tree = transform(new_tree)
+        
+        ns = {'nfe': 'http://www.synchro.com.br/nfe'}
+
+        rps_element = transformed_tree.find('.//nfe:RPS', namespaces=ns)
+
+        for key, value in custom_tags.items():
+            element = etree.Element(key)
+            element.text = str(value)
+            rps_element.append(element)
+
+        output_file = 'out/transformed_{}.xml'.format(index)
+        with open(output_file, 'wb') as f:
+            f.write(etree.tostring(transformed_tree,
+                    pretty_print=True, xml_declaration=True))
